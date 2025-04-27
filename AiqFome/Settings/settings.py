d887784d21f380edad18be407e25d764e8a52d26
Fernando Amorim <fer.gamorim@icloud.com>
2025-04-27 13:34:32 -0300

    change debug option in Settings

--- AiqFome/Settings/settings.py
+++ AiqFome/Settings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*8vxlj30i08ce3j7yx-gxe*^rkn7wkb=kl-#y#--vct7&2bu)i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
